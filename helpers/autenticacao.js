4e0a118c2ed646b90f23081ffaf1e88e0dc612b6
Nicolas Duarte <nicolascoutoduarte@gmail.com>
2025-09-19 19:15:08 -0300

    Adicionando fixtures para gerenciar o corpos da requisições

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,14 +1,14 @@
 const request = require('supertest')
 require('dotenv').config()
+const postLogin = require("../fixtures/postLogin.json")
 
 const obterToken = async (usuario, senha) => {
+     const bodyLogin = { ...postLogin  }
+
     const resposta = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({
-            'username': usuario,
-            'senha': senha,
-        })
+        .send(bodyLogin)
 
     return resposta.body.token
 }
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require("../helpers/autenticacao.js")
-
+const postTranferencias = require("../fixtures/postTranferencias.json")
 describe('Tranferências', () => {
     describe('POST /transferencias', () => {
 
@@ -13,31 +13,25 @@ describe('Tranferências', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de 10 reais', async () => {
+            const bodyTransferencias = { ...postTranferencias }
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
+                .send(bodyTransferencias)
             expect(resposta.status).to.equal(201);
 
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de 10 reais', async () => {
+            const bodyTransferencias = { ...postTranferencias }
+            bodyTransferencias.valor = 9
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 9,
-                    token: ""
-                })
+                .send(bodyTransferencias)
             expect(resposta.status).to.equal(422);
         })
     })
